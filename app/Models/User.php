817338710b32459526664a0d53eb1937005d560a
nebiyat <nebiyatfikresilasse@gmail.com>
2026-07-07 09:58:57 -0700

    Day 5: Created Post model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,3 +30,16 @@ class User extends Authenticatable
         ];
     }
 }
+
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'content'];
+}
