fdab235e7b3a563b2d7398705743c8dc331b3491
nebiyat <nebiyatfikresilasse@gmail.com>
2026-07-07 08:32:24 -0700

    Day 1: Project setup

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use illluminate\http\request;
+class PageController extends Controller
+{
+    //
+}
+
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+<h1>About Page</h1>
+<p>Hello neva, this is my first custom Laravel page!</p>
+</body>
+</html>
+
+
